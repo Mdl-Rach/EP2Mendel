c89c66f158f7ff656433c3f4a2361bd75964b505
Mdl-Rach <mendyrach18@gmail.com>
2025-04-28 13:13:01 -0300

    Questão 3

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,11 @@ def guardar_dado (lista_mesa, lista_guardado, n_dado):
     lista_final.append(lista_mesa)
     lista_final.append(lista_guardado)
     return lista_final
+# Questão 3
+def remover_dado (lista_dados_rolados, lista_dados_guardados, n_dado):
+    lista_final = []
+    lista_dados_rolados.append(lista_dados_guardados[n_dado])
+    del lista_dados_guardados[n_dado]
+    lista_final.append(lista_dados_rolados)
+    lista_final.append(lista_dados_guardados)
+    return lista_final
